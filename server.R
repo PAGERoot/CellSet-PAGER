465708945a3b853fe8a620b5e8ddbcb4cb19460a
guillaumelobet <g.lobet@fz-juelich.de>
2017-01-25 14:17:34 +0100

    Added RSML support and better UI

--- server.R
+++ server.R
@@ -1,20 +1,59 @@
-# Guillaume Lobet - University of Liege
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
 #
-# http://shiny.rstudio.com
+# Copyright © 2017, Université catholique de Louvain
+# All rights reserved.
+# 
+# Copyright © 2017 Forschungszentrum Jülich GmbH
+# All rights reserved.
+# 
+# Developers: Guillaume Lobet
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted under the GNU General Public License v3 and provided that the following conditions are met:
+#   
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+# 
+# Disclaimer
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# You should have received the GNU GENERAL PUBLIC LICENSE v3 with this file in license.txt but can also be found at http://www.gnu.org/licenses/gpl-3.0.en.html
+# 
+# NOTE: The GPL.v3 license requires that all derivative work is distributed under the same license. That means that if you use this source code in any other program, you can only distribute that program with the full source code included and licensed under a GPL license.
+
+
+# This is the server side of the Shiny app to transform CellSet output files (XLS)
+# into one single RSML file that can be read by PAGE-Root
+
+
 
 library(shiny)
 
 shinyServer(
-  function(input, output, clientData, session) {
-
+  
+  function(input, output, clientData, session) {  
+    
+    dataset <- reactiveValues(rep = NULL)
+    
+    # For the root paramerers
     observe({
+      if(is.null(dataset$rep)){return()}
+      
+      dt <- dataset$rep
+      ts <- as.character(unique(dt$cell_type))
+      message(ts)
+      
+      for(i in c(1:length(cell_types))){
+        updateSelectInput(session, paste0("type",i), choices = ts, selected = ts[pmatch(substr(cell_types[i], 0, 3), ts)])
+      }
+      
+    })    
+        
+    
+    observeEvent(input$load_data, {
             
-      if(input$load_data == 0){return()}
-
-
         #------------------------------------------------------
         # LOAD THE USER DATA
         #------------------------------------------------------
@@ -22,44 +61,207 @@ shinyServer(
         # Load datafiles
         pathData <- paste0(input$dirPath, "/")
         
-        if (is.null(pathData)) return(NULL)
-        
-        withProgress(message = 'WORKING:', value = 0, {
-          
-          
-          
-          # Attach the reporter informations
-          list.f <- list.files(pathData)
+        # if (is.null(pathData)) return(NULL)
 
+        # Attach the reporter informations
+        #pathData <- "../test_data/small_dataset/"
+        message(pathData)
+        list.f <- list.files(pathData)
+        nfiles <<- length(list.f)
+        
+        withProgress(message = 'Loading data', {
           rs <- NULL
           j <- 1
           for(f in list.f){
-            message(round(j/length(list.f), 2))
-            incProgress(round(j/length(list.f), 2), detail = paste0("Loading the file ",f))
+            j <- j+1
+            incProgress(j/length(list.f), detail = paste0("Loading the file ",f))
             name <- gsub(".xlsx", "", f)
             for(i in 1:20){
               tryCatch({
                 temp <- read_excel(paste0(pathData, f), sheet = i)
                 temp <- temp[!is.na(temp[,1]),]
                 # Normalize the fluorescence data
-                fluo <- scale(temp[["Average flourescence"]])
+                fluo <- temp[["Average flourescence"]]
+                #fluo <- scale(temp[["Average flourescence"]])
                 rs <- rbind(rs, data.frame(line=name, root=i, cell_type=temp$Label, value=fluo))
               },warning = function(w) {
               }, error = function(e) {
               })
             }
-            j <- j+1
           }
-          remove(temp, i, f, list.f, name)
         })
         
-        write.csv(rs, paste0(input$dirSave, "/reporter-data.csv"))
+        remove(temp, i, f, list.f, name)
+        rs <- rs[!is.na(rs$value),]
+        
+        rs$id <- rep(c(1:5), ceiling(nrow(rs)/3))[1:nrow(rs)]
+        rs <- ddply(rs, .(line, root, cell_type, id), summarize, value=mean(value))
+        
+        message(nrow(rs))
+        
+        dataset$rep <- rs
         
     })
-    
-    
-    output$results <- renderText({ 
-      if(input$load_data == 0){return()}
-      "Conversion done"
+        
+    observeEvent(input$update_data, {
+      rs <- dataset$rep
+      rs$cell_type[rs$cell_type == input$type1] <- cell_types[1]
+      rs$cell_type[rs$cell_type == input$type2] <- cell_types[2]
+      rs$cell_type[rs$cell_type == input$type3] <- cell_types[3]
+      rs$cell_type[rs$cell_type == input$type4] <- cell_types[4]
+      rs$cell_type[rs$cell_type == input$type5] <- cell_types[5]
+      rs$cell_type[rs$cell_type == input$type6] <- cell_types[6]
+      rs$cell_type[rs$cell_type == input$type7] <- cell_types[7]
+      dataset$rep <- rs
     })
+    
+    # ----------------------------------------------------------------
+    # Downlaod the data
+    # ----------------------------------------------------------------
+    
+    # downloadHandler() takes two arguments, both functions.
+    # The content function is passed a filename as an argument, and
+    #   it should write out data to that filename.
+    output$save_data <- downloadHandler(
+      
+
+      
+      # This function returns a string which tells the client
+      # browser what name to use when saving the file.
+      filename = function() {
+        if(is.null(dataset$rep)) return()
+        if(length(unique(dataset$rep$cell_type)) != length(cell_types)) return()
+        
+        "experession.rsml"
+      },
+      
+      # This function should write data to a file given to it by
+      # the argument 'file'.
+      content = function(file) {
+        
+        
+        
+        withProgress(message = 'Saving data', {
+          
+          rs <- dataset$rep
+          
+          message(unique(rs$cell_type))
+          
+          rs$cell_type[rs$cell_type == input$type1] <- cell_types[1]
+          rs$cell_type[rs$cell_type == input$type2] <- cell_types[2]
+          rs$cell_type[rs$cell_type == input$type3] <- cell_types[3]
+          rs$cell_type[rs$cell_type == input$type4] <- cell_types[4]
+          rs$cell_type[rs$cell_type == input$type5] <- cell_types[5]
+          rs$cell_type[rs$cell_type == input$type6] <- cell_types[6]
+          rs$cell_type[rs$cell_type == input$type7] <- cell_types[7]
+          
+          message(unique(rs$cell_type))
+          # Write down th RSML file to store the data
+          rsmlText <- "<?xml version='1.0' encoding='UTF-8'?>\n"
+          rsmlText <- paste0(rsmlText, "\t<rsml xmlns:po='http://www.plantontology.org/xml-dtd/po.dtd'>\n")
+          
+          # Fill in the metadata
+          rsmlText <- paste0(rsmlText, "\t\t<metadata>\n")
+          rsmlText <- paste0(rsmlText, "\t\t\t<version>1</version>\n")
+          rsmlText <- paste0(rsmlText, "\t\t\t<unit>inch</unit>\n")
+          rsmlText <- paste0(rsmlText, "\t\t\t<resolution>300.0</resolution>\n")
+          rsmlText <- paste0(rsmlText, "\t\t\t<last-modified>",Sys.Date(),"</last-modified>\n")
+          rsmlText <- paste0(rsmlText, "\t\t\t<software>PAGE-Root</software>\n")
+          rsmlText <- paste0(rsmlText, "\t\t\t<file-key>",sha1(rs),"</file-key>\n")
+          
+          # Get the properties (in this case, we set a property for each cell type
+          rsmlText <- paste0(rsmlText, "\t\t\t<property-definitions>\n")
+          for(cell in unique(rs$cell_type)){
+            rsmlText <- paste0(rsmlText, "\t\t\t\t<property-definition>\n")
+            rsmlText <- paste0(rsmlText, "\t\t\t\t\t<label>",cell,"</label>\n")
+            rsmlText <- paste0(rsmlText, "\t\t\t\t\t<type>numeric</type>\n")
+            rsmlText <- paste0(rsmlText, "\t\t\t\t\t<unit>-</unit>\n")
+            rsmlText <- paste0(rsmlText, "\t\t\t\t</property-definition>\n")
+          }
+          
+          rsmlText <- paste0(rsmlText, "\t\t\t</property-definitions>\n")
+          rsmlText <- paste0(rsmlText, "\t\t</metadata>\n")
+          
+          # Let's start wrting the scene object, that will contain all the information fo all the roots
+          rsmlText <- paste0(rsmlText, "\t\t<scene>\n")
+          i <- 0
+          for(l in unique(rs$line)){  
+            i <- i+1
+            incProgress(round(1/length(unique(rs$line)), 2), detail = paste0("Saving the line ",l))
+            
+            # We create one "plant" per line / genotype present in the original file
+            rsmlText <- paste0(rsmlText, "\t\t\t<plant id='", i, "' label = '", l, "' >\n")
+            j <- 0
+            for(r in unique(rs$root[rs$line == l])){
+              j <- j+1 
+              # We create one root object for each root. This root object wll the contain the fluorescence data
+              rsmlText <- paste0(rsmlText, "\t\t\t\t<root id='",i,"-",j,"' label='",r,"' po:accession='PO:0009005'>\n")
+              rsmlText <- paste0(rsmlText, "\t\t\t\t\t<geometry></geometry>\n")
+              rsmlText <- paste0(rsmlText, "\t\t\t\t\t<functions></functions>\n")
+              rsmlText <- paste0(rsmlText, "\t\t\t\t\t<annotations>\n")
+              temp <- rs[rs$line == l & rs$root == r, ]
+              # We store the fluorescence data as annotations for the root, since they
+              # are not associated with any root geometry. Here, we use one type of annotation by
+              # of cell line
+              for(cell in unique(temp$cell_type)){
+                rsmlText <- paste0(rsmlText, "\t\t\t\t\t\t<annotation name='",cell,"'>\n")
+                n <- nrow(temp[temp$cell_type == cell,])
+                fluo <- paste0(rep("\t\t\t\t\t\t\t<value>", n),
+                                     round(temp$value[temp$cell_type == cell], 5),
+                                     rep("</value>\n", n), collapse="")
+                rsmlText <- paste0(rsmlText, fluo)
+                rsmlText <- paste0(rsmlText, "\t\t\t\t\t\t</annotation>\n")
+              }
+              rsmlText <- paste0(rsmlText, "\t\t\t\t\t</annotations>\n")
+              # }
+              rsmlText <- paste0(rsmlText, "\t\t\t\t</root>\n")
+            }
+            rsmlText <- paste0(rsmlText, "\t\t\t</plant>\n")
+          }
+          rsmlText <- paste0(rsmlText, "\t\t</scene>\n")
+          rsmlText <- paste0(rsmlText, "\t</rsml>")
+          cat(rsmlText, file="~/Desktop/test.rsml")
+          
+          message(paste0(nrow(rs), " observations \n", nfiles, " data files"))
+          remove(temp, cell, fluo, i, j, l, n, r)
+  
+          cat(rsmlText, file=file)
+        })
+    })
+        
+    
+    
+    output$obs <- renderText({ 
+      if (is.null(dataset$rep)) { return()}
+      paste0(nrow(dataset$rep), " observations were uploaded")
+    })
+    
+    output$check <- renderText({ 
+      if (is.null(dataset$rep)) { return()}
+      if(length(unique(dataset$rep$cell_type)) == length(cell_types)){
+        "Number of cell types is OK"
+      }else{
+        ""
+      }
+    }) 
+    output$notcheck <- renderText({ 
+      if (is.null(dataset$rep)) { return()}
+      if(length(unique(dataset$rep$cell_type)) == length(cell_types)){
+        ""
+      }else{
+        "More cell types in the dataset than allowed. Please update your datafile"
+      }
+    })     
+    
+    output$lines <- renderText({ 
+      if (is.null(dataset$rep)) { return()}
+        paste(unique(dataset$rep$line), sep=", ", collapse =", ")
+    })
+    
+    output$table_results <- renderTable({
+      if (is.null(dataset$rep)) { return()}
+      dataset$rep[1:5,]
+    })  
+    
+    
 })
--- ui.R
+++ ui.R
@@ -1,35 +1,84 @@
-# Guillaume Lobet - University of Liege
+# Copyright © 2017, Université catholique de Louvain
+# All rights reserved.
+# 
+# Copyright © 2017 Forschungszentrum Jülich GmbH
+# All rights reserved.
+# 
+# Developers: Guillaume Lobet
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted under the GNU General Public License v3 and provided that the following conditions are met:
+#   
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+# 
+# Disclaimer
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# You should have received the GNU GENERAL PUBLIC LICENSE v3 with this file in license.txt but can also be found at http://www.gnu.org/licenses/gpl-3.0.en.html
+# 
+# NOTE: The GPL.v3 license requires that all derivative work is distributed under the same license. That means that if you use this source code in any other program, you can only distribute that program with the full source code included and licensed under a GPL license.
 
+# This is the user interface of the Shiny app to transform CellSet output files (XLS)
+# into one single RSML file that can be read by PAGE-Root
 
 library(shiny)
 
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel(h1("--| CellSet to PAGE-Root |--")),
+  titlePanel("--| CellSet to PAGE-Root |--"),
   
   fluidRow(
-    column(3, wellPanel(
-      helpText("Small app to transform data coming from CellSet into a format the PAGER will understand"),
+    column(3, 
+      helpText("Transform data coming from CellSet into a single RSML file"),
+        wellPanel(
+        h4("1. Where are your xlsx files"),
+        textInput('dirPath', "Enter the name of folder containing the reporter xlsx files", f1, placeholder = "Select folder"),
+        actionButton(inputId = "load_data", label="Load your data",icon("upload"))
+        )
+      ),
+    
+    column(3,
+           h4("3. View your data"),
+           tableOutput('table_results'),
+           tags$hr(),
+           h5("Lines in the file"),
+           textOutput("lines"),
+           tags$hr(),
+           textOutput("obs")
+           
+    ), 
+  
+    column(3,wellPanel(
+      h4("4. Set your cell types"),
+      selectInput("type1", label = cell_types[1], choices = "Load files"),
+      selectInput("type2", label = cell_types[2], choices = "Load files"),
+      selectInput("type3", label = cell_types[3], choices = "Load files"),
+      selectInput("type4", label = cell_types[4], choices = "Load files"),
+      selectInput("type5", label = cell_types[5], choices = "Load files"),
+      selectInput("type6", label = cell_types[6], choices = "Load files"),
+      selectInput("type7", label = cell_types[7], choices = "Load files"),
+      h4(textOutput("more")),
+      actionButton(inputId = "update_data", label="Update your data")
+      
+    )),
+    
+    column(3, 
+           div(htmlOutput("check"), style="color:green"),
+           div(htmlOutput("notcheck"), style="color:red"),
+           tags$hr(),
+           wellPanel(
+             h4("5. Save the data file?"),
+             downloadButton("save_data", "Download RSML")
+           )
+      
+      )
+    
 
-      h3("1. Where are your xlsx files"),
-      tags$hr(),      
-      
-      
-      textInput('dirPath', "Enter the name of folder containing the reporter xlsx files", placeholder = "Select folder"),
-      tags$hr(), 
-      
-      h3("2. Where to save the data file?"),
-      
-      textInput('dirSave', "Enter the name of folder where to save the data file", placeholder = "Select folder"),
-
-      actionButton(inputId = "load_data", label="Load and transform your data"),
-      
-      tags$hr(), 
-      
-      h4(textOutput("results"))
-      
-      
-      ))  
+    
     )
 ))
